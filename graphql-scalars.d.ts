ff97347827b04d311e66e4bc236ad4d5fb743152
Jiri Spac <capajj@gmail.com>
2019-02-13 01:40:04 +0100

    fixes typings

--- graphql-scalars.d.ts
+++ graphql-scalars.d.ts
@@ -1,24 +1,37 @@
 import { GraphQLScalarType } from 'graphql';
 
-declare namespace GraphqlScalars {
-  declare const EmailAddress: GraphQLScalarType;
-  declare const DateTime: GraphQLScalarType;
-  declare const NonPositiveInt: GraphQLScalarType;
-  declare const PositiveInt: GraphQLScalarType;
-  declare const NonNegativeInt: GraphQLScalarType;
-  declare const NegativeInt: GraphQLScalarType;
-  declare const NonPositiveFloat: GraphQLScalarType;
-  declare const PositiveFloat: GraphQLScalarType;
-  declare const NonNegativeFloat: GraphQLScalarType;
-  declare const NegativeFloat: GraphQLScalarType;
-  declare const EmailAddress: GraphQLScalarType;
-  declare const URL: GraphQLScalarType;
-  declare const PhoneNumber: GraphQLScalarType;
-  declare const PostalCode: GraphQLScalarType;
+export declare const EmailAddress: GraphQLScalarType;
+export declare const DateTime: GraphQLScalarType;
+export declare const NonPositiveInt: GraphQLScalarType;
+export declare const PositiveInt: GraphQLScalarType;
+export declare const NonNegativeInt: GraphQLScalarType;
+export declare const NegativeInt: GraphQLScalarType;
+export declare const NonPositiveFloat: GraphQLScalarType;
+export declare const PositiveFloat: GraphQLScalarType;
+export declare const NonNegativeFloat: GraphQLScalarType;
+export declare const NegativeFloat: GraphQLScalarType;
+export declare const URL: GraphQLScalarType;
+export declare const PhoneNumber: GraphQLScalarType;
+export declare const PostalCode: GraphQLScalarType;
 
-  declare class RegularExpression extends GraphQLScalarType {
-    constructor(name: string, regex: RegExp);
-  }
+export declare class RegularExpression extends GraphQLScalarType {
+  constructor(name: string, regex: RegExp);
 }
 
-export = GraphqlScalars;
+export declare const DateTimeScalar: string;
+export declare const NonPositiveIntScalar: string;
+export declare const PositiveIntScalar: string;
+export declare const NonNegativeIntScalar: string;
+export declare const UnsignedIntScalar: string;
+export declare const NegativeIntScalar: string;
+export declare const NonPositiveFloatScalar: string;
+export declare const PositiveFloatScalar: string;
+export declare const NonNegativeFloatScalar: string;
+export declare const UnsignedFloatScalar: string;
+export declare const NegativeFloatScalar: string;
+export declare const EmailAddressScalar: string;
+export declare const URLScalar: string;
+export declare const PhoneNumberScalar: string;
+export declare const PostalCodeScalar: string;
+export declare const RegularExpressionScalar: string;
+export declare const OKGScalarDefinitions: string[];
